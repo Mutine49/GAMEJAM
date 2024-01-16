594f80e7faf70c91f0766b989c1764e58d623b3d
DESKTOP-5R6HITJ\Etudiant <arthus.dubois@lecole-ldlc.com>
2024-01-16 11:41:00 +0100

    Monster Steps

---
+++ Assets/Waypoint.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Waypoint : MonoBehaviour
+{
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.blue;
+        Gizmos.DrawSphere(transform.position, 1f);
+    }
+}
