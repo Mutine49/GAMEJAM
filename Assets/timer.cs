d366b30d802c50efe61a6346c4394d4c01b76e47
DESKTOP-NISPQC6\etudiants4 <etudiant@lecole.com>
2024-01-16 16:18:28 +0100

    le timer

---
+++ Assets/timer.cs
@@ -0,0 +1,28 @@
+using TMPro;
+using UnityEngine;
+
+public class timer : MonoBehaviour
+{
+    TMP_Text textObject;
+    float timers = 0f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        textObject = GetComponent<TMP_Text>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timers += Time.deltaTime;
+        textObject.text = "Heure : " + AdjustTime(timers);
+    }
+
+    string AdjustTime(float time)
+    {
+        int h = (int)(time / 60);
+        int m = (int)(time % 60);
+
+        return $"{h:00}:{m:00}";
+    }
+}
