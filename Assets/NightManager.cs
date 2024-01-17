3a60b8ddfd2554de905dc4bb8cb96946af2ad93c
DESKTOP-NISPQC6\etudiants4 <etudiant@lecole.com>
2024-01-17 13:49:14 +0100

    night manager + main menu

---
+++ Assets/NightManager.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class NightManager : MonoBehaviour
+{
+    public static NightManager instance;
+    public static NightManager Instance
+    {
+        get
+        {
+              return instance;
+        }
+    }
+
+    public Action nightStopped;
+    public Action<int> newNightId;
+
+    [SerializeField] timer tObject;
+    [SerializeField] List<Graphic> graphics;
+    [SerializeField] TMP_Text text;
+    public int currentNight;
+    // Start is called before the first frame update
+    void Start()
+    {
+        if(instance != null)
+        {
+            Destroy(instance);
+            return;
+        }
+
+        instance = this;
+        tObject.callback += NextNight;
+        currentNight = -1;
+        NextNight();
+    }
+
+    void SetColorOfAllGraphics(float a)
+    {
+        foreach (var g in graphics)
+        {
+            Color c = g.color;
+            c.a = a;
+            g.color = c;
+        }
+    }
+
+    IEnumerator FadeCoroutine()
+    {
+        float timer = 0.0f;
+        float fadeIn = 2.5f;
+        while(timer <= fadeIn)
+        {
+            timer += Time.deltaTime; 
+            foreach(var g in graphics)
+            {
+                SetColorOfAllGraphics(Mathf.Lerp(0f, 1f, timer / fadeIn));
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        SetColorOfAllGraphics(1f);
+        yield return new WaitForSeconds(3f);
+        timer = 0f;
+        while (timer <= fadeIn)
+        {
+            timer += Time.deltaTime;
+            foreach (var g in graphics)
+            {
+                SetColorOfAllGraphics(Mathf.Lerp(1f, 0f, timer / fadeIn));
+            }
+            yield return new WaitForEndOfFrame();
+        }
+        SetColorOfAllGraphics(0f);
+        tObject.ResetTimer();
+        newNightId.Invoke(currentNight);
+        yield return null;
+    }
+
+    void NextNight()
+    {
+        nightStopped.Invoke();
+        currentNight++;
+        text.text = "Night " + (currentNight + 1);
+        StartCoroutine(FadeCoroutine());
+    }
+}
--- Assets/timer.cs
+++ Assets/timer.cs
@@ -1,21 +1,57 @@
+using System;
 using TMPro;
 using UnityEngine;
 
 public class timer : MonoBehaviour
 {
+    [SerializeField] float triggerTime = 360;
+    public Action callback;
     TMP_Text textObject;
     float timers = 0f;
+    bool started;
     // Start is called before the first frame update
     void Start()
     {
         textObject = GetComponent<TMP_Text>();
     }
 
+    public void StartTimer()
+    {
+        started = true;
+    }
+
+    public void StopTimer()
+    {
+        started = false;
+        timers = 0f;
+    }
+
+    public void PauseTimer()
+    {
+        started = false;
+    }
+
+    public void ResetTimer()
+    {
+        timers = 0f;
+        started = true;
+    }
+
     // Update is called once per frame
     void Update()
     {
-        timers += Time.deltaTime;
-        textObject.text = AdjustTime(timers);
+        if(started)
+        {
+            timers += Time.deltaTime;
+            if(timers >= triggerTime)
+            {
+                timers = triggerTime;
+                PauseTimer();
+                callback?.Invoke();
+            }
+            textObject.text = AdjustTime(timers);
+        }
+
     }
 
     string AdjustTime(float time)
